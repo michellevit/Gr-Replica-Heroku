b91e0eabbe25240b2ebd5b3837e2e203d182f7b3
michellevit <mvflandin@gmail.com>
2024-06-01 00:09:28 -0700

    Add user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
-# class User < ApplicationRecord
-#     # Validations
-#     validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-#     validates :password, presence: true
-#     validates :balance, numericality: { greater_than_or_equal_to: 0 }
+class User < ApplicationRecord
+    # Validations
+    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :password, presence: true
+    validates :balance, numericality: { greater_than_or_equal_to: 0 }
   
-#     # Optional associations (if you have relationships with other models)
-#     # For example, if a user has many links:
-#     # has_many :links, dependent: :destroy
+    # Optional associations (if you have relationships with other models)
+    # For example, if a user has many links:
+    # has_many :links, dependent: :destroy
   
-#     # Add any necessary callbacks or methods here
-# end
+    # Add any necessary callbacks or methods here
+end
