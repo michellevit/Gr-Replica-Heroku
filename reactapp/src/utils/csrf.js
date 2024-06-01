b13ff5c08ae13887fc417c810fbe00cb6003f561
michellevit <mvflandin@gmail.com>
2024-05-31 23:12:40 -0700

    Remove csrf token

--- reactapp/src/utils/csrf.js
+++ reactapp/src/utils/csrf.js
@@ -1,4 +1,5 @@
 export const getCsrfToken = () => {
   const token = document.querySelector('meta[name="csrf-token"]') || document.querySelector('#root').dataset.csrfToken;
+  console.log('CSRF Token:', token ? token.getAttribute('content') : 'Token not found');
   return token ? token.getAttribute('content') : '';
 };
