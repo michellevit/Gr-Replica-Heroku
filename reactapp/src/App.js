125a107fa56c325a75e2c7c8aa24006c1b11a0da
michellevit <mvflandin@gmail.com>
2024-05-31 23:47:57 -0700

    Remove double router

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Header from "./nav/Header";
 import Footer from "./nav/Footer";
 import Home from "./pages/Home";
@@ -37,7 +37,6 @@ function App() {
 
   return (
     <CsrfProvider>
-      <Router>
         <div className="app">
           <div className="top-bar"></div>
           <div id="wrapper">
@@ -63,7 +62,6 @@ function App() {
           </div>
           <Footer />
         </div>
-      </Router>
     </CsrfProvider>
   );
 }
